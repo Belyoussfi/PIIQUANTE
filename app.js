e766c77a12f615d821634e409bdff7c6242bbced
Belyoussfi <elyoussfi.bouchera@gmail.com>
2021-08-31 13:05:07 +0200

    Implémentation des routes/controllers/models pour  auth et sauces

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser =require('body-parser');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauces');
+
+mongoose.connect('mongodb+srv://bouchera:Bouchera20.@cluster0.w7upl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();  
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
+
+      
+module.exports = app;
---
+++ controllers/sauces.js
@@ -0,0 +1,39 @@
+const Sauces = require('../models/Sauces');
+const fs = require('fs');
+
+exports.getAllSauces = (req, res, next) => {
+  Sauces.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+  Sauces.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+};
+
+exports.createSauce = (req, res, next) => {
+    delete req.body._id;
+    const sauce = new Sauces({
+      ...req.body
+    });
+    sauce.save()
+      .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+      .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifySauce = (req, res, next) => {
+    Sauces.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauces.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+};
+
+
+
